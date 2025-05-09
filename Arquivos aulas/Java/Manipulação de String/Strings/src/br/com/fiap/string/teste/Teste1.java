cef58a8877f6d43cb6041a344dc7f098790c94d4
rodrigueszkkk <kaikyauthentic@gmail.com>
2025-05-09 19:29:44 -0300

    aula8 java

---
+++ Arquivos aulas/Java/Manipulação de String/Strings/src/br/com/fiap/string/teste/Teste1.java
@@ -0,0 +1,10 @@
+package br.com.fiap.string.teste;
+
+public class Teste1 {
+
+    public static void main(String[] args) {
+        //declarar uma String
+
+        String nome = "Kaiky";
+    }
+}
