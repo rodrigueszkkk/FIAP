921be22f25948d15aa6e69ba6ad51b26a4d72cd5
rodrigueszkkk <kaikyauthentic@gmail.com>
2025-05-12 20:54:45 -0300

    heranca pt1

--- Arquivos aulas/Java/Apostila09/src/br/com/fiap/esporte/model/PessoaFisica.java
+++ Arquivos aulas/Java/Apostila09/src/br/com/fiap/esporte/model/PessoaFisica.java
@@ -1,6 +1,22 @@
 package br.com.fiap.esporte.model;
 
-public class PessoaFisica {
-    String cpf;
-    boolean aposentado;
+public class PessoaFisica extends Pessoa {
+    private String cpf;
+    private boolean aposentado;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public boolean isAposentado() {
+        return aposentado;
+    }
+
+    public void setAposentado(boolean aposentado) {
+        this.aposentado = aposentado;
+    }
 }
