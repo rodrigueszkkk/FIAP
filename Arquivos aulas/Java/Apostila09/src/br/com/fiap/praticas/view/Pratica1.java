5c06e93110d9967283e74c8f7be6b0917c85ca77
rodrigueszkkk <kaikyauthentic@gmail.com>
2025-05-16 23:10:20 -0300

    ex-java

--- Arquivos aulas/Java/Apostila09/src/br/com/fiap/praticas/view/Pratica1.java
+++ Arquivos aulas/Java/Apostila09/src/br/com/fiap/praticas/view/Pratica1.java
@@ -1,13 +1,21 @@
 package br.com.fiap.praticas.view;
 
+import br.com.fiap.praticas.model.Aviao;
 import br.com.fiap.praticas.model.Carro;
+import br.com.fiap.praticas.model.Lancha;
+
+import java.util.Scanner;
 
 public class Pratica1 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
 
         Carro carro = new Carro();
-        
+        Aviao aviao = new Aviao();
+        Lancha lancha = new Lancha();
+
+        carro.set
 
     }
 }
