646cc1a7e94e58b70e8b78477a75bdfe327fc095
rodrigueszkkk <kaikyauthentic@gmail.com>
2025-05-18 20:57:33 -0300

    cadastro de produtos

--- Arquivos aulas/Python/projetosReais/cadastro.py
+++ Arquivos aulas/Python/projetosReais/cadastro.py
@@ -1,7 +1,39 @@
-print('Deseja: \n 1- Cadastrar \n 2- remover')
+def menu() -> int:
+    print('\n =====SISTEMA PDV=====')
+    print('1- Cadastrar Produto')
+    print('2- Ver Produtos Cadastrados')
+    print('3- Encerrar Programa')
+    return int(input('\n Qual opcao deseja? '))
+
 
 cadastros = ()
 
 def cadastro():
-    cod = int(print('digite o codigo do produto: '))
+
+    name = str(input('Digite a descricao do produto: '))
+    cod = int(input('digite o codigo do produto: ')) 
+
+    produtos.append(name)
+    produtos.append(cod)
+
+
+def lista():
+    print('=====LISTA DE PRODUTOS JA CADASTRADOS===== \n')
+
+
+    for i in produtos:
+        print (i)
+
+
+produtos = []
+opcao = 0
+
+while opcao != 3:
+    opcao = menu()
+    if opcao == 1:
+        cadastro()
+    if opcao == 2:
+        lista()
+    if opcao == 3:
+        exit
 
